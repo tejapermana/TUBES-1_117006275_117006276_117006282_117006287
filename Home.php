4dee30d9a557546326ec7a5e605865fd00546975
RifkiWirawan <muhamad.rifki@student.unsil.ac.id>
2014-05-04 00:28:57 +0700

    Create Home.php

---
+++ Home.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<head>
+	<title> Tugas Matalakuliah Layanan Web </title>
+<hr>
+</head> 
+<body>
+<div class="warppercenter">
+   
+<div id="content">
+  <table width="85%" border="0" cellspacing="0" cellpadding="0">
+    <tr>
+      <td valign="top">
+      <div id="content_left">
+        <div id="left_menu">
+        <div id="left_menu_top"><br><br>      
+          <div id="left_menu_title">Nama Anggota Kelompok :</div>
+	  </div>
+		
+	   <ul>
+		<li><a href="https://github.com/HidrianOmaSuharman">Hidrian Oma Suharman (117006282) </a></li> 
+		<li><a href="https://github.com/RifkiWIrawan">M. Rifki Wirawan (117006287)</a></li>
+		<li><a href="https://github.com/tejapermana">Teja Permana (117006275) </a></li> 
+		<li><a href="https://github.com/rensasuryaanggara">Rensa Surya Anggara (117006276)</a></li>
+		<a href="https://github.com/HidrianOmaSuharman/TUBES-1_117006275_117006276_117006282_117006287/network"><h3>Network Graph</h3></a>	
+	   </ul>
+<td valign="top">
+	      <div id="content_right">
+<div class=description2>
+   <h1 style='padding-top: 10px;'>HOME</h1><div class='descalign'> 
+ 	<span><br>
+	</span></div> </div>
+	<div align=center style="padding-top:20px;">
+</div>
+
+<!DOCTYPE html>
+<head>
+  <table width="100%" border="0" cellpadding="0" cellspacing="4" bgcolor="#e6ecf6">
+	 </form><tr><td colspan=2><table class=result width=100% height=100%>
+<tr><td>
+<div style=float:left></div>
+</td></tr>
+
+<tr><td><div align=left style=padding: 18px;></div>
+		</div>
+		<form name=first>
+		<table border=0 cellpadding=12 cellspacing=0>
+		<tr align=center></tr>
+		<tr align=center><td>
+		</td>
+		<?php
+		include("index.php");
+		?>
+		</form>
+	</table>
+</table>
+</div>
+
+</body>
+</html>
